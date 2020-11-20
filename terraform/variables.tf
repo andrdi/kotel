f585cfa293a6ce09b8d64861592005b7083bad17
Andrey Lygin <a.lygin@compassplus.com>
2020-11-20 23:13:27 +0500

    New AWS free account

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "availability_zone" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "access_key" {
+  type = string
+  default = "my-access-key"
+}
+
+variable "secret_key" {
+  type = string
+  default = "my-secret-key"
+}
