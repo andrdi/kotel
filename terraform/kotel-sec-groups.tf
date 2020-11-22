04019e52358e580885497c777aa34ee94994c35a
Andrey Lygin <a.lygin@compassplus.com>
2020-11-23 02:01:01 +0500

    Ansible OpenVPN

--- terraform/kotel-sec-groups.tf
+++ terraform/kotel-sec-groups.tf
@@ -1,12 +1,19 @@
 resource "aws_security_group" "KotelSG" {
   name        = "KotelSG"
-  description = "Allow SSH traffic"
+  description = "Allow SSH and OpenVPN traffic"
   vpc_id      = aws_vpc.kotelVPC.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
